245f163546576650751091dd466d9e10999e643b
Esteban <es.alamos@duocuc.cl>
2024-10-02 17:40:04 -0300

    validacion en modificar perfil listo

--- src/app/pages/modificarperfil/modificarperfil.page.ts
+++ src/app/pages/modificarperfil/modificarperfil.page.ts
@@ -9,10 +9,57 @@ import { AlertController } from '@ionic/angular';
 })
 export class ModificarperfilPage implements OnInit {
 
+  nombremodificarperfil: string = "";
+  apellidomodificarperfil: string = "";
+  telefonomodificarperfil: string = "";
+  correomodificarperfil: string = "";
+  
   constructor(private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
+  ValidacionModificarPerfil(){
+    if (this.nombremodificarperfil.trim() === '') {
+      this.presentAlert('Error', 'Por favor, complete todos los campos requeridos.');
+      return; // Salir de la función si algún campo está vacío
+    }
+  
+    // Hacemos la validación de los datos
+    if (this.isFormValid()) {
+      // Si el formulario es válido, muestra un mensaje de éxito
+      this.presentAlert('Realizado', 'Perfil Modificado con exito');
+      this.VolveralInicio(); // Navegar a la página de inicio si el registro es exitoso
+    } else {
+      // Si el formulario es inválido, muestra un mensaje de error en la alerta
+      this.presentAlert('Error', 'Datos inválidos, por favor revise los datos ingresados.');
+    }
+  }
+
+  isFormValid(): boolean {
+    const regex = /^[a-zA-Z]+$/; // Solo letras
+    const regexPhone = /^\d{1,9}$/; // Solo números y hasta 9 dígitos
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Expresión regular para el correo electrónico
+
+    return (
+      this.nombremodificarperfil.trim() !== '' && // No debe estar vacío
+      this.nombremodificarperfil.length >= 2 &&
+      this.nombremodificarperfil.length <= 10 &&
+      regex.test(this.nombremodificarperfil) &&
+
+      this.apellidomodificarperfil.trim() !== '' && // Apellido no debe estar vacío
+      this.apellidomodificarperfil.length >= 2 && 
+      this.apellidomodificarperfil.length <= 10 && 
+      regex.test(this.apellidomodificarperfil) && // Validación del apellido
+
+      this.telefonomodificarperfil.trim() !== '' && // Teléfono no debe estar vacío
+      regexPhone.test(this.telefonomodificarperfil) && // Validación del teléfono
+
+      this.correomodificarperfil.trim() !== '' && // Correo no debe estar vacío
+      regexEmail.test(this.correomodificarperfil)  // Validación del correo
+
+    );
+  }
+
   async presentAlert(titulo:string, msj:string) {
     const alert = await this.alertController.create({
       header: titulo,
@@ -21,10 +68,6 @@ export class ModificarperfilPage implements OnInit {
     });
 
     await alert.present();
-  }
-  Confirmarcambios(){
-    //aqui hacemos las validaciones del formulario
-    this.presentAlert('Realizado','Perfil Modificado con exito');
   }
   VolveralInicio(){
     let navigationextras: NavigationExtras = {
