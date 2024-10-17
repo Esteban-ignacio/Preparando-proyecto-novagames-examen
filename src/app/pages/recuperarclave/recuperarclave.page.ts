7c7200df9e83f472d9006fc6473ff40e7c0a8725
Esteban <es.alamos@duocuc.cl>
2024-10-16 21:34:35 -0300

    mejora visual en recuperar contraseña

--- src/app/pages/recuperarclave/recuperarclave.page.ts
+++ src/app/pages/recuperarclave/recuperarclave.page.ts
@@ -10,8 +10,6 @@ import { AlertController } from '@ionic/angular';
 export class RecuperarclavePage implements OnInit {
 
   correorecuperarclave: string = "";
-  contrasenarecuperarclave: string = "";
-  confirmarContrasenarecuperarclave: string = "";
 
   constructor(private alertController: AlertController, private router: Router) { }
 
@@ -19,22 +17,21 @@ export class RecuperarclavePage implements OnInit {
   }
 
   ValidacionRecuperarClave(){
-    if (this.correorecuperarclave.trim() === ''|| this.contrasenarecuperarclave.trim() === '' || this.confirmarContrasenarecuperarclave.trim() === '') {
+    if (this.correorecuperarclave.trim() === '') {
       this.presentAlert('Error', 'Por favor, complete todos los campos requeridos.');
       return; // Salir de la función si algún campo está vacío
     }
 
       // Validar correo, contraseña y confirmar contraseña con alertas específicas
-      if (!this.isCorreoRecuperarClaveValido() || !this.isContrasenaRecuperarClaveValida()
-        || !this.isConfirmarContrasenaRecuperarClaveValida()) {
+      if (!this.isCorreoRecuperarClaveValido()) {
           return; // Si alguno de los campos es inválido, no continuar
         }
   
     // Hacemos la validación de los datos
     if (this.isFormValid()) {
       // Si el formulario es válido, muestra un mensaje de éxito
-      this.presentAlert('Logrado','Contraseña recuperada');
-      this.RegresarLogin(); // Navegar a la página de inicio si el registro es exitoso
+      this.presentAlert('Acceso aprobado','Ingrese los datos para cambiar su contraseña');
+      this.Ircambiarcontra(); // Navegar a la página de inicio si el registro es exitoso
     } else {
       // Si el formulario es inválido, muestra un mensaje de error en la alerta
       this.presentAlert('Error', 'Datos inválidos, por favor revise los datos ingresados.');
@@ -51,39 +48,12 @@ export class RecuperarclavePage implements OnInit {
   return true;
 }
 
- // Validación para la contraseña
- isContrasenaRecuperarClaveValida(): boolean {
-  const regexPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{5,10}$/; // Contraseña con al menos una mayúscula, un número y un símbolo
-
-  if (!regexPassword.test(this.contrasenarecuperarclave)) {
-    this.presentAlert('Error', 'La contraseña debe tener entre 5 y 10 caracteres, incluir al menos una letra mayúscula, un número y un símbolo.');
-    return false;
-  }
-  return true;
-}
-
-// Validación para confirmar contraseña
-isConfirmarContrasenaRecuperarClaveValida(): boolean {
-  if (this.confirmarContrasenarecuperarclave !== this.contrasenarecuperarclave) {
-    this.presentAlert('Error', 'La contraseña no coincide.');
-    return false;
-  }
-  return true;
-}
-
   isFormValid(): boolean {
     const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // Expresión regular para el correo electrónico
-    const regexPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{5,10}$/; // Contraseña con mayúscula, número y símbolo
-
+    
     return (
       this.correorecuperarclave.trim() !== '' &&// Correo no debe estar vacío
-      regexEmail.test(this.correorecuperarclave) && // Validación del correo
-
-      this.contrasenarecuperarclave.trim() !== '' && // Contraseña no debe estar vacía
-      regexPassword.test(this.contrasenarecuperarclave) && // Validación de la contraseña
-
-      this.confirmarContrasenarecuperarclave.trim() !== '' && // Confirmar contraseña no debe estar vacía
-      this.confirmarContrasenarecuperarclave === this.contrasenarecuperarclave // Debe coincidir con la contraseña
+      regexEmail.test(this.correorecuperarclave)  // Validación del correo
 
     );
   }
@@ -104,4 +74,12 @@ isConfirmarContrasenaRecuperarClaveValida(): boolean {
     }
     this.router.navigate(['/login'], navigationextras);
   }
+
+  Ircambiarcontra(){
+    let navigationextras: NavigationExtras = {
+      
+    }
+    this.router.navigate(['/cambiarclave'], navigationextras);
+  }
+
 }
