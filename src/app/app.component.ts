f684bfd953872483f1195d7b6f9e2709dab83b63
Esteban <es.alamos@duocuc.cl>
2024-11-12 18:16:06 -0300

    menu de admin listo e integrado a las paginas que usan el menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ServiceBDService } from './service/service-bd.service';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,15 @@ import { Router } from '@angular/router';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private bdService: ServiceBDService, private router: Router) {}
+  constructor(private bdService: ServiceBDService, private router: Router, private menuCtrl: MenuController) {}
+
+  navegarYCerrar(ruta: string) {
+    // Navega a la página seleccionada
+    this.router.navigate([ruta]).then(() => {
+      // Cierra el menú después de la navegación
+      this.menuCtrl.close('menu-admin');
+    });
+  }
 
   // Función para manejar el cierre de sesión
   async cerrarSesion() {
