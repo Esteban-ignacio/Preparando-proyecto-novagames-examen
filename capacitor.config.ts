24a7284cf332fc4e4722acda075de96c50c3c2b5
Esteban <es.alamos@duocuc.cl>
2024-11-18 17:59:50 -0300

    diseño en registro, perfil, modificar perfil y tamaño de la foto de perfil en perfil

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'videojuegos',
+  appName: 'NovaGames',
   webDir: 'www'
 };
 
