a117401e528a05aa4dd6a8541d5618012e1f2c54
Esteban <es.alamos@duocuc.cl>
2024-11-19 22:17:25 -0300

    arreglando errores en clase compra y productos.spect.ts para pruebas unitarias

--- src/app/service/compra.ts
+++ src/app/service/compra.ts
@@ -6,20 +6,6 @@ export class Compra {
     nombre_prod!: string;        // Nombre del producto comprado
     cantidad_prod!: number;      // Cantidad del producto comprado
 
-    constructor(
-        id_compra: number, 
-        v_venta: number, 
-        total_compra: number,  
-        correo_usuario: string, 
-        nombre_prod: string, 
-        cantidad_prod: number
-    ) {
-        this.id_compra = id_compra;
-        this.v_venta = v_venta;
-        this.total_compra = total_compra;
-        this.correo_usuario = correo_usuario;
-        this.nombre_prod = nombre_prod;
-        this.cantidad_prod = cantidad_prod;
-    }
+    
 }
 
--- src/app/service/productos.spec.ts
+++ src/app/service/productos.spec.ts
@@ -2,6 +2,16 @@ import { Productos } from './productos';
 
 describe('Productos', () => {
   it('should create an instance', () => {
-    expect(new Productos()).toBeTruthy();
+    const producto = new Productos(
+      1, // id_prod
+      'Nombre de prueba', // nombre
+      'Descripción de prueba', // descripcion
+      'imagen.jpg', // imagen_prod
+      100, // precio
+      10, // stock
+      1 // cantidad
+    );
+    expect(producto).toBeTruthy();
   });
 });
+
