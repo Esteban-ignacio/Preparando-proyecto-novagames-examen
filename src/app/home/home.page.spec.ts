51ac09f711a3e07f3647545a2d571bc6069d957d
Esteban <es.alamos@duocuc.cl>
2024-11-28 18:08:46 -0300

    arreglo en prueba unitaria en home sobre activerouter, funciona pero hay dudas si esta bien

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { RouterTestingModule } from '@angular/router/testing'; // No debe estar tachado
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { HomePage } from './home.page';
 
@@ -10,7 +13,24 @@ describe('HomePage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomePage],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        RouterTestingModule, // Incluye el módulo de pruebas de rutas
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({
+              get: (key: string): string | null => {
+                if (key === 'id') return 'mockValue'; // Simulación del parámetro 'id'
+                return null;
+              },
+            }),
+            queryParams: of({}), // Simula los parámetros de consulta si los necesitas
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
@@ -22,3 +42,5 @@ describe('HomePage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+
