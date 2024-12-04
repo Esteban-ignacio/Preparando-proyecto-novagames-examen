f0eab1acaad9fad4dba9b9fac3d4730519c12a9a
Esteban <es.alamos@duocuc.cl>
2024-12-04 19:37:36 -0300

    pruebas unitarias 3,4,5 y 6 en modificarperfil, validacion correcta e incorrecta de nombre, apellido y telefono

--- src/app/pages/modificarperfil/modificarperfil.page.spec.ts
+++ src/app/pages/modificarperfil/modificarperfil.page.spec.ts
@@ -20,4 +20,35 @@ describe('ModificarperfilPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //Prueba unitaria 3, validar campo nombre y apellido sean correctos
+  it('debería validar correctamente el nombre y el apellido', () => {
+    component.nombremodificarperfil = 'Juan';
+    component.apellidomodificarperfil = 'Perez';
+    const resultado = component.isNombreApellidoModificarPerfilValido();
+    expect(resultado).toBeTrue();
+  });
+
+  //Prueba unitaria 4, validar campo nombre y apellido sean incorrectos
+  it('debería mostrar error para nombre o apellido inválido', () => {
+    component.nombremodificarperfil = 'J1'; // Inválido
+    component.apellidomodificarperfil = 'Perez';
+    const resultado = component.isNombreApellidoModificarPerfilValido();
+    expect(resultado).toBeFalse();
+  });
+
+  //Prueba unitaria 5, validar campo telefono sea correcto
+  it('debería validar correctamente el teléfono', () => {
+    component.telefonomodificarperfil = '123456789';
+    const resultado = component.isTelefonoModificarPerfilValido();
+    expect(resultado).toBeTrue();
+  });
+
+  //Prueba unitaria 6, validar campo telefono sea incorrecto
+  it('debería mostrar error para teléfono inválido', () => {
+    component.telefonomodificarperfil = '12345'; // Inválido
+    const resultado = component.isTelefonoModificarPerfilValido();
+    expect(resultado).toBeFalse();
+  });
+
 });
